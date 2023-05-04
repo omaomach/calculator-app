6936cdcc076c12a834121b75d81533d8f857e2d8
omaomach <aaronjoash44@gmail.com>
2023-05-04 16:21:39 +0300

    feat: created Calculator class

---
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -0,0 +1,56 @@
+package com.example.calculator
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Calculator(
+    state: CalculatorState,
+    buttonSpacing: Dp = 8.dp,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(buttonSpacing)
+        ) {
+            Text(
+                text = state.num1 + (state.operation ?: "" + state.num2),
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                fontWeight = FontWeight.Light,
+                fontSize = 80.sp,
+                color = Color.White,
+                maxLines = 2
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ) {
+                CalculatorButton(
+                    symbol = "AC",
+                    modifier = Modifier
+                        .background()
+                ) {
+
+                }
+
+            }
+        }
+    }
+    
+}
--- app/src/main/java/com/example/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -8,7 +8,7 @@ import androidx.lifecycle.ViewModel
 class CalculatorViewModel: ViewModel() {
 
     var state by mutableStateOf(CalculatorState())
-    private set
+        private set
 
 
 }
